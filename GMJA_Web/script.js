30ec4f66620dae68be006e3fe60902dbff8b608e
Dananoo <104047024+Dananoo@users.noreply.github.com>
2025-11-21 23:04:47 +0900

    Delete GMJA_Web directory

--- GMJA_Web/script.js
+++
@@ -1,9 +0,0 @@
-function copyCA() {
-    const caInput = document.getElementById('ca-input');
-    caInput.select();
-    caInput.setSelectionRange(0, 99999); /* For mobile devices */
-    navigator.clipboard.writeText(caInput.value);
-    alert("Copied the Holy Address!");
-}
-
-console.log('Meme Coin site loaded');
